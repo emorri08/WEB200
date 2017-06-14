bdeaf0f0f14c1b4dde0f451a87f7b8167043aaac
Elly <emorri08@seattlecentral.edu>
2017-06-14 00:47:02 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+//Elly Boyd
+//WEB200
+//Protosite
+//script.js
 
+//navigation toggle
+
+$(function(){
+    $("#navigation").menumaker({
+    title: "Menu",
+    breakpoint: 768,
+    format: "select"
+    });
+    
+});
