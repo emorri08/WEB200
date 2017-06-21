fb6cede625250d479f5f0d841753b35b6da210d3
Elly <emorri08@seattlecentral.edu>
2017-06-21 07:39:54 -0700

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,14 @@
         <meta name="robots" content="noindex,nofollow" />
                 <title><?=$title?></title>
             <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-            <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+   <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
             <script src="js/script.js"></script><!-- Yeah, Moe, that team sure did suck last night! They just plain sucked! I've seen teams suck before, but they were the suckiest bunch of sucks that ever sucked! -Homer Simpson -->
         <link rel="stylesheet" href="css/style.css" />
         <link rel="stylesheet" href="css/navigation.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Prompt:700" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
     </head>
 <!-- end invisible content -->
     <body>
@@ -35,88 +36,56 @@
             
 <!-- begin navigation -->
             
-<nav id="navigation">
-<ul>
-     <li>
-         <a href="index.php">Home</a>
-    </li>
-    <li>
-         <a href="about.php">About</a>
-        
-            <ul>
-                <li>
-                    <a href="resume.php">Resume</a>
-                </li>
-                <li>
-                    <a href="testemonials.php">Testemonials</a>
-                </li>
-                <li>
-                    <a href="press.php">Press</a>
-                </li>
-                <li>
-                    <a href="privacy.php">Privacy</a>
-                </li>
-                <li>
-                    <a href="safety.php">Safety</a>
-                </li>
-                <li>
-                    <a href="events.php">Events</a>
-                </li>
-                <li>
-                    <a href="faq.php">FAQ</a>
-                </li>
-        </ul>
-          
-    </li>
-    <li>
-        <a href="services.php">Services</a>
-        
-            <ul>
-                <li>
-                    <a href="monitoring.php">Medical Monitoring</a>
-                </li>
-                <li>
-                    <a href="protocol.php">Protocol Writing</a>
-                </li>
-                <li>
-                    <a href="dsmb.php">DSMB</a>
-                </li>
-                <li>
-                    <a href="presentations.php">Presentations</a>
-                </li>
-                <li>
-                    <a href="studies.php">Phase 1 Studies</a>
-                </li>
-                <li>
-                    <a href="participant-faq.php">Study Participant FAQ</a>
-                </li>
-            </ul>
-        
-    </li>
-    <li>
-        <a href="resources.php">Resources</a>
-    </li>
-    <li>
-        <a href="blog.php">Blog</a>
-        
-        <ul>
-           <li>
-               <a href="white-papers.php">White Papers</a>
-            </li>
-           <li>
-               <a href="articles.php">Articles</a>
-            </li>
-           <li>
-               <a href="archives.php">Archives</a>
-            </li>
-        </ul>
-        
-     </li>
-     <li>
-         <a href="contact.php">Contact</a>
-    </li>
-</ul>
-</nav>
+            <div id="mainNav">
+                <ul>
+                    <li><a href="index.php"><span>HOME</span></a></li>
+                    
+                    <li><a href="about.php"><span>ABOUT</span></a>
+                        
+                        <ul>
+                            <li><a href="resume.php"><span>Resume</span></a></li>
+                            
+                            <li><a href="testemonials.php"><span>Testemonials</span></a></li>
+                            
+                            <li><a href="press.php"><span>Press</span></a></li>
+                            
+                            <li><a href="privacy.php"><span>Privacy</span></a></li>
+                            
+                            <li><a href="faq.php"><span>FAQ</span></a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="services.php"><span>SERVICES</span></a>
+                        <ul>
+                            <li><a href="monitoring.php"><span>Medical Monitoring</span></a></li>
+                            
+                            <li><a href="protocol.php"><span>Protocol Writing</span></a></li>
+                            
+                            <li><a href="dsmb.php"><span>DSMB</span></a></li>
+                            
+                            <li><a href="presentations.php"><span>Presentations</span></a></li>
+                            
+                            <li><a href="studies.php"><span>Phase 1 Studies</span></a></li>
+                            
+                            <li><a href="participant-faq.php"><span>Study Participant FAQ</span></a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="resources.php"><span>RESOURCES</span></a></li>
+                    
+                    <li><a href="blog.php"><span>BLOG</span></a>
+                        <ul>
+                            <li><a href="white-papers.php"><span>White Papers</span></a></li>
+                            
+                            <li><a href="articles.php"><span>Articles</span></a></li>
+                            
+                            <li><a href="archives.php"><span>Archives</span></a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="contact.php"><span>CONTACT</span></a></li>
+                </ul>
+            </div>
             
 <!-- end navigation -->
             
