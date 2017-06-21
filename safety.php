9d1f09e0b00043481009da67b490aa9a160fc6a0
Elly <emorri08@seattlecentral.edu>
2017-06-21 08:09:06 -0700

    Add files via upload

--- safety.php
+++ safety.php
@@ -13,43 +13,23 @@
 <!-- start main content -->
             <div id="pageContent">
                 <article id="fullArticleOne">
-                    <h2 id="articleHeadline" class="headline">Article Main Headline</h2>
+                    <h2 id="articleHeadline" class="headline">Safety Practices</h2>
                     
                     <p id="byLine">Author Name, mm/dd/yyyy, email@address.com</p>
                     
                         <p id="first" class="articleContent">Bring her forward! We want a shrubbery!! What do you mean? What do you mean? You don't frighten us, English pig-dogs! Go and boil your bottoms, sons of a silly person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English K-n-n-n-n-n-n-n-niggits! Why do you think that she is a witch? You don't frighten us, English pig-dogs! Go and boil your bottoms, sons of a silly person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English K-n-n-n-n-n-n-n-niggits! The Knights Who Say Ni demand a sacrifice! Did you dress her up like this? Why do you think that she is a witch? And the hat. She's a witch! I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time!
                         </p>
                     
-                            <h3 id="firstSubHead" class="subHeading">First Sub-Headline</h3>
-                                <div id="listOne" class="list">
-                                    <ol>
-                                        <li>You don't vote for kings.</li>
-                                        <li>Well, how'd you become king, then?</li>
-                                        <li>Oh, ow!</li>
-                                    </ol>
-                                </div>
                     
                         <p class="articleContent">He hasn't got shit all over him. Oh, ow! I dunno. Must be a king. …Are you suggesting that coconuts migrate? Well, I got better. Well, what do you want? And this isn't my nose. This is a false one. <em>Shut up!</em> Will you shut up?! Shut up! Well, I got better. And the hat. She's a witch! How do you know she is a witch? …Are you suggesting that coconuts migrate? But you are dressed as one… Ah, now we see the violence inherent in the system! I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time! She looks like one. No, no, no! Yes, yes. A bit. But she's got a wart. We want a shrubbery!! We found them. I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time! Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.
                         </p>
                     
-                            <h3 id="secondSubHead" class="subHeading">Second Sub-Headline</h3>
-                                <div id="listTwo" class="list">
-                                    <ul>
-                                        <li>Be quiet!</li>
-                                        <li>Found them? In Mercia?! The coconut's tropical!</li>
-                                        <li>We shall say 'Ni' again to you, if you do not appease us.</li>
-                                    </ul>
-                                </div>
                       
                         <p class="articleContent">I have to push the pram a lot. You don't frighten us, English pig-dogs! Go and boil your bottoms, sons of a silly person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English K-n-n-n-n-n-n-n-niggits! …Are you suggesting that coconuts migrate? How do you know she is a witch? Well, what do you want? I dunno. Must be a king. Why do you think that she is a witch? The Knights Who Say Ni demand a sacrifice! And this isn't my nose. This is a false one. What do you mean? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Now, look here, my good man. How do you know she is a witch? Look, my liege! And this isn't my nose. This is a false one. Well, how'd you become king, then? Look, my liege! Well, Mercia's a temperate zone! And this isn't my nose. This is a false one. I'm not a witch. Now, look here, my good man. You don't vote for kings. Well, how'd you become king, then?
                         </p>                  
-                            <div id="quoteOne">
-                                <blockquote>Did you dress her up like this? We want a shrubbery!! I'm not a witch. Bring her forward! Well, how'd you become king, then? A newt?
-                                </blockquote>
-                            </div>
+                            
                         <p class="articleContent">There's no Messiah in here. There's a mess all right, but no Messiah. Now go away! Manacles! Ooooh, my idea of heaven, is to be allowed to be put in manacles. Just for a few hours. They must think <em>the sun shines out your ass</em>, sonny.Well, I think I should point out first, Brian, in all fairness, we are not, in fact, the rescue committee. However, I have been asked to read the following prepare statement on behalf of the movement. &quot;We the People's Front of Judea, brackets, officials, end brackets, do hereby convey our sincere fraternal and sisterly greetings to you, Brian, on this, the occasion of your martyrdom.&quot; Stwike him, Centuwion! Stwike him vewy wuffly! At this time, a friend shall lose his friend's hammer and the young shall not know where lieth the things possessed by their fathers that their fathers put there only just the night before, about eight o'clock. All right, but apart from the sanitation, medicine, education, wine, public order, irrigation, roads, the fresh water system and public health, what have the Romans ever done for us? Oh, what wouldn't I give to be spat at in the face? I sometimes hang awake at night, dreaming of being spat at in the face.
                         </p>
-                    <h3 id="thirdSubHead" class="subHeading">Third Sub-Headline</h3>
                         <p id="last" class="articleContent">I'm not a roman mum, I'm a kike, a yid, a heebie, a hook-nose, I'm kosher mum, I'm a Red Sea pedestrian, and proud of it! I will not have my fwends widiculed by the common soldiewy. Anybody else feel like a little... giggle... when I mention my fwiend... Biggus... Dickus? Hey! Your nose is going to be three foot wide across your face by the time I'm finished with you! This is boring. <a href="#">Let's go watch a stoning</a>. Well, er, yes Mr. Anchovy, but you see your report here says that you are an extremely dull person. You see, our experts describe you as an appallingly dull fellow, unimaginative, timid, lacking in initiative, spineless, easily dominated, no sense of humour, tedious company and irrepressibly drab and awful. And whereas in most professions these would be considerable drawbacks, in chartered accountancy, they're a positive boon. And Dinsdale says 'I hear you've been a naughty boy, Clement', and he splits me nostrils open, saws me leg off and pulls me liver out. And I tell him 'My name's not Clement', and then he loses his temper and nails my head to the floor.
                         </p>
                     
